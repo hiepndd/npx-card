e2a07a69810ef507a0e5b8ce703207079648701c
hiepndd <nguyendangdaihiep@gmail.com>
2019-04-27 14:47:40 +0700

    fix some error related to dependencies

--- build.js
+++ build.js
@@ -16,22 +16,22 @@ const data = {
   handle: chalk.white('hiepndd'),
   work: chalk.white('A Gopher who is using VIM'),
   from: chalk.white('Ho Chi Minh city, Vietnam'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('hiepndd'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~hiepndd'),
-  github: chalk.gray('https://github.com/') + chalk.green('hiepndd'),
+  twitter: chalk.white('https://twitter.com/') + chalk.green('hiepndd'),
+  npm: chalk.white('https://npmjs.com/') + chalk.green('~hiepndd'),
+  github: chalk.white('https://github.com/') + chalk.green('hiepndd'),
   linkedin:
-    chalk.gray('https://linkedin.com/in/') +
+    chalk.white('https://linkedin.com/in/') +
     chalk.green('hiep-nguyen-16012311a'),
   facebook:
-    chalk.gray('https://facebook.com/') +
-    chalk.blue('profile.php?id=100006888487736'),
-  npx: chalk.red('npx') + ' ' + chalk.white('hiepndd'),
+    chalk.white('https://facebook.com/') +
+    chalk.green('profile.php?id=100006888487736'),
+  npx: chalk.white('npx') + ' ' + chalk.green('hiepndd'),
   labelWork: chalk.white.bold('   Title:'),
   labelFrom: chalk.white.bold('    From:'),
   labelTwitter: chalk.white.bold(' Twitter:'),
   labelnpm: chalk.white.bold('     npm:'),
   labelGitHub: chalk.white.bold('  GitHub:'),
-  labelLinkedIn: chalk.white.bold('LinkedIn:'),
+  labelLinkedIn: chalk.white.bold('Linkedin:'),
   labelFacebook: chalk.white.bold('Facebook:'),
   labelCard: chalk.white.bold('    Card:'),
 };
